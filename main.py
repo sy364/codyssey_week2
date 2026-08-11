ab54e59f23dd79be59145516a1c2073d5d4a3975
kimseyun <seyoon0604@gmail.com>
2026-08-11 17:31:09 +0900

    feat:힌트 기능 추가

--- main.py
+++ main.py
@@ -3,10 +3,11 @@ import random
 
 # 개별 퀴즈
 class Quiz:
-    def __init__(self,question,choices,answer):
-        self.question=question
-        self.choices=choices
-        self.answer=answer
+    def __init__(self, question, choices, answer, hint="힌트가 없습니다."):
+        self.question = question
+        self.choices = choices
+        self.answer = answer
+        self.hint = hint
 
     def print_quiz(self):
         print(self.question)
@@ -59,6 +60,7 @@ class QuizGame:
                 "question" : q.question,
                 "choices" : q.choices,
                 "answer" : q.answer
+                "hint": getattr(q, 'hint', "힌트가 없습니다.")
             })
 
         data_to_save={
@@ -87,10 +89,13 @@ class QuizGame:
                 break
         while True:
             answer_input=input("정답번호1-4를 입력하세요: ").strip()
-
+            hint = input("힌트를 입력하세요 (없으면 그냥 엔터): ").strip()
+   
             if not answer_input:
                 print("입력값이 없습니다. 다시 입력해주세요. ")
                 continue
+            if not hint:
+                hint = "힌트가 없습니다."
 
             try:
                 answer =int(answer_input)
@@ -101,7 +106,7 @@ class QuizGame:
             except ValueError:
                 print("숫자만 입력 가능합니다. 다시 입력해주세요.")
 
-            new_quiz=Quiz(question,choices, answer)
+            new_quiz=Quiz(question,choices, answer,hint)
             self.quizzes.append(new_quiz)
 
             self.save_data()
@@ -129,36 +134,50 @@ class QuizGame:
                     print(f"1에서 {total_available} 사이의 숫자를 입력해주세요.")
             except ValueError:
                 print("숫자만 입력 가능합니다.")
-                
-        print("\n===퀴즈 풀기 시작===")
-        current_score=0
-        total_quizzes=len(self.quizzes)
 
-        for i,quiz in enumerate(self.quizzes,1):
-            print(f"\n[문제 {i}/{total_quizzes}]{quiz.question}")
 
-            for j,choice in enumerate(quiz.choices,1):
-                print(f" {j}.{choice}")
+            selected_quizzes=random.sample(self.quizzes,num_questions)
 
-            while True:
-                answer_input=input("정답 번호(1~4)를 입력하세요: ").strip()
-                if not answer_input:
-                    print("입력값이 없습니다. 다시 입력해주세요. ")
-                    continue
+            current_score=0
 
-                try:
-                    user_answer=int(answer_input)
-                    if 1<=user_answer<=4:
-                        break
-                    else:
-                        print("1에서 4 사이의 번호만 입력 가능합니다.")
+            for i,quiz in enumerate(selected_quizzes,1):
+                print(f"\n[문제{i}/{num_questions}]{quiz.question}")
+                for j, choice in enumerate(quiz.choices,1):
+                    print(f"{j}.{choice}")
 
-                except ValueError:
-                    print("숫자만 입력 가능합니다. 다시 입력해주세요.")
+                hint_used = False
+                while True:
+                    answer_input=input("정답 번호(1~4)를 입력하세요(힌트 보기: h): ").strip().lower()
+                    if not answer_input:
+                                        print("입력값이 없습니다. 다시 입력해주세요. ")
+                                        continue
 
+                    if answer_input=='h':
+                        if not hint_used:
+                            print(f"\n[힌트]{quiz.hint}")
+                            hint_used=True
+                        else:
+                            print("이미 힌트를 사용했습니다. ")
+                            continue
+                    
+                    try:
+                        user_answer=int(answer_input)
+                        if 1<=user_answer<=4:
+                            break
+                        else:
+                            print("1에서 4 사이의 번호만 입력 가능합니다.")
+    
+                    except ValueError:
+                        print("숫자만 입력 가능합니다. 다시 입력해주세요.")
+
+        
                 if quiz.checkAnswer(user_answer):
                     print("정답입니다!")
-                    current_score+=1
+                    if hint_used:
+                        print("(힌트 사용으로 0.5점만 인정됩니다.)")
+                        current_score += 0.5
+                    else:
+                        current_score += 1
                 else:
                     print(f"오답입니다. 정답은 {quiz.answer}번입니다. ")
 
